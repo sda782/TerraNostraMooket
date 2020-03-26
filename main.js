403a678dedb9264e8379c12e552dce36e58e18a2
sda782 <59613370+sda782@users.noreply.github.com>
2020-03-26 12:07:37 +0100

    updated UI

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     console.log("site loaded");
     // Getting shop info from Json file
-    $.getJSON("https://raw.githubusercontent.com/sda782/TerraNostraMooket/master/shop_index.json", function(shopindex) {
-        //console.log(shopindex);
-        $.each(shopindex, function(i, shop) {
+    $.getJSON("https://raw.githubusercontent.com/sda782/TerraNostraMooket/master/shop_index.json", function (shopindex) {
+        $.each(shopindex, function (i, shop) {
 
             var stock = "";
-            $.each(shop.inventory, function(j, inv) {
-                console.log(inv.name);
-                stock = stock + inv.amount + ' ' + inv.name + ' for ' + inv.price + '<br>';
+            $.each(shop.inventory, function (j, inv) {
+                stock = '<p>' + stock + inv.amount + ' ' + inv.name + ' for ' + inv.price + '</p>';
             });
-            $('#market').append('<div class="shop"><h2 id="shopname">' + shop.name + '</h2><h5 id="owner">' + shop.owners + '</h5><div class="stock">' + stock + '</div></div>');
+            $('#market').append('<div class="shop"><h2 id="shopname">' + shop.name + '</h2><h5 id="owner">' + shop.owners + '</h5><button onclick="seeStock(this)">Show stock</button><div class="stock">' + stock + '</div></div>');
         });
     });
+
 });
+
+function seeStock(stockID) {
+    $(stockID).parent().children('.stock').toggle();
+}
